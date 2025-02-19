5a9dec9497db23b9b5af8b108c2673d99cc902a0
Menny Even Danan <mennyevendanan@asana.com>
2025-02-19 16:07:38 -0500

    interceptions

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,28 @@
-function init(modules: { typescript: typeof import("typescript/lib/tsserverlibrary") }) {
-    const ts = modules.typescript;
-  
-    function create(info: ts.server.PluginCreateInfo) {
-      // Get a list of things to remove from the completion list from the config object.
-      // If nothing was specified, we'll just remove 'caller'
-      const whatToRemove: string[] = info.config.remove || ["caller"];
+import type * as Ts from "typescript/lib/tsserverlibrary";
 
+function init({typescript: ts} : { typescript: typeof Ts }) {
+    function create(info:  Ts.server.PluginCreateInfo) {
       // Diagnostic logging
-      info.project.projectService.logger.info(
-        "I'm getting set up now! Check the log for this message."
-      );
+      info.project.projectService.logger.info("Loading our ts-server plugin POC");
   
       // Set up decorator object
-      const proxy: ts.LanguageService = Object.create(null);
-      for (let k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
-        const x = info.languageService[k]!;
+      const proxy: Ts.LanguageService = Object.create(null);
+      for (const k of Object.keys(info.languageService) as Array<keyof Ts.LanguageService>) {
+        const x = info.languageService[k];
         // @ts-expect-error - JS runtime trickery which is tricky to type tersely
         proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args);
       }
   
-      // Remove specified entries from completion list
-      proxy.getCompletionsAtPosition = (fileName, position, options) => {
-        // This is just to let you hook into something to
-        // see the debugger working
-        debugger        
-
-        const prior = info.languageService.getCompletionsAtPosition(fileName, position, options);
-        if (!prior) return
-
-        const oldLength = prior.entries.length;
-        prior.entries = prior.entries.filter(e => whatToRemove.indexOf(e.name) < 0);
-  
-        // Sample logging for diagnostic purposes
-        if (oldLength !== prior.entries.length) {
-          const entriesRemoved = oldLength - prior.entries.length;
-          info.project.projectService.logger.info(
-            `Removed ${entriesRemoved} entries from the completion list`
-          );
-        }
-  
-        return prior;
-      };
-  
+      info.languageService.getProgram()?.getGlobalDiagnostics()
+      // printing options
+      let interceptions = "\n";
+      for (const k of Object.keys(info.languageService) as Array<keyof Ts.LanguageService>) {
+        const funcValue = info.languageService[k];
+        if (funcValue === undefined) continue;
+        const fullImpl = funcValue.toString();
+        interceptions += `- func ${k}: ${fullImpl.substring(0, fullImpl.indexOf('{')).trim()}\n`;
+      }
+      info.project.projectService.logger.info(`ts-server plugin POC has the following interception points:${interceptions}`);
       return proxy;
     }
   
@@ -49,4 +30,4 @@ function init(modules: { typescript: typeof import("typescript/lib/tsserverlibra
   }
   
   export = init;
-  
+
